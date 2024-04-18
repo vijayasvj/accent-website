6c4f6b7383cb913d60622bf7c1759384f369d6fc
vijayasvj <vj.as.entrepreneur@gmail.com>
2024-04-19 01:57:48 +0530

    Final Frontend commit

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify, request
 import numpy as np
 from scipy.spatial.distance import cosine
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000'])
 
 @app.route('/generate_random_number', methods=['GET'])
 def generate_random_number():
@@ -21,11 +23,10 @@ def pad_array(arr, length):
 def compare_audios():
     # Get the audio arrays from the request
     data = request.json
-    audio1 = data['audio1']
-    id = data['id']
-    enc=audio1.b64encode(open("file.wav").read())
+    audio1 = data['audio']
+    id = data['testId']
     random_number = random.randint(0, 10)  # Generating a random number between 0 and 10
     return jsonify({'score':random_number})
                     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 3001)
--- src/constants/dummyTestData.js
+++ src/constants/dummyTestData.js
@@ -1,42 +1,82 @@
 export const dummyTestData = [
   {
     id: 1,
-    text: "The quick brown fox jumps over the lazy dog.",
+    text: "a gold ring with please most any girl the long journey home took a year she saw a cat in the neighbours house a pink shell was found on the Sandy beach small children came to see him the grass and bushes were wet with do the blind man counted his old coins Storms store down the bar she called his name many times when you hear the bell come quickly",
   },
-  //   {
-  //       id: 2,
-  //       text: "Pack my box with five dozen liquor jugs."
-  //   },
-  //   {
-  //       id: 3,
-  //       text: "How can a clam cram in a clean cream can?"
-  //   },
-  //   {
-  //       id: 4,
-  //       text: "The big black bear sat on a big black rug."
-  //   },
-  //   {
-  //       id: 5,
-  //       text: "Two driven jocks help fax my big quiz."
-  //   },
-  //   {
-  //       id: 6,
-  //       text: "Sixty zippers were quickly picked from the woven jute bag."
-  //   },
-  //   {
-  //       id: 7,
-  //       text: "Cozy lummox gives smart squid who asks for job pen."
-  //   },
-  //   {
-  //       id: 8,
-  //       text: "The five boxing wizards jump quickly."
-  //   },
-  //   {
-  //       id: 9,
-  //       text: "Fix, Schwyz! quäkt Jürgen blöd vom Paß."
-  //   },
+  {
+    id: 2,
+    text: "fill the ink chart with sticky glue he smoke a big pipe but strong contacts we need going to keep our meals helping packed the records in I need send case the crunch of feet in the snow was the only sound the copper ball shown in the sun's race boat and last kept dry the first chair line against the ball class book by metal base and relax in the cool green grass"
+  },
+  {
+    id: 3,
+    text: "the birds can use split on the smooth things clear background makes fine punch the box was done by the park trucks a large size is starting"
+  },
+  {
+    id: 4,
+    text: "slide the box into the empty space the plant and green in the window The Beam dropped down on the workmen's head pink clouds loaded with the breeze she danced like us one talent graceful the tube was blown in the tired flat in the morning on the old clock be seeing the last course the last switch cannot be turned as fight 6 minutes"
+  },
+  {
+    id: 5,
+    text: "the smallest twisted in turn on the bed Hulk press the pencil so but not the best one I was for a perfect the beauty of the views on the young boy swim in the think writer the same morning"
+  },
+  {
+    id: 6,
+    text: "the boy was there when the sun rose is used to catch pink feminine the source of the huge river is a clear spring kick the pulse rate and power through how to get back to her feet a part of tea how to pass the evening smokey fire is like flame and heat the soft question 50 pounds"
+  },
+  {
+    id: 7,
+    text: "a pencil with black layer Wright best quotes a young cat to drink from a bucket schools for ladies teacher and grace the lamp shown with a steady green flame they took three x and the saw to the forest the ancient coin was quite a doll and won't like fast music director find to find ribbons"
+  },
+  {
+    id: 8,
+    text: "who is the love to your left shoulder take the winding path to reach the lake no closely the size of the gas tank white decrease office dirty face before you go out the rest was very strange in home lamp the strategy for chickens the young girl gave no clear responsible for the beloving water is a living"
+  },
+  {
+    id: 9,
+    text: "I can grow the state in the early days the cheapest one apart on the sharp leaf igneous chapter him home delivery in the heart son the way to tell a in the program live score by in the seven books on one sentence love story"
+  },
   {
     id: 10,
-    text: "Jackdaws love my big sphinx of quartz.",
+    text: "paint the sockets in the world dog green the child called into the dance grass bribes fail were honest man work trample The Spark of the Flames will spread the health of the sword was carved was signed designs around hole was drilled through the same board footprints show the path he took up the beach she was waiting at my front line event brought in pressure district"
   },
-]
+  // {
+  //   id: 11,
+  //   text: "the boy was there when the sun was used to catch pink diamond the source of the huge river is the clear spring kick the fastest running smoking first Black flame in heat the sound"
+  // },
+  // {
+  //   id: 12,
+  //   text: "the new struck out into the restless mind once we started be decided at the Shore a chunk in the wall allowed to draught to blow fashion tops on each side a cold storage health and just he takes the Olive office each March the sand drift over the sale of the old house the point of the Steel Plant was bent in twisted there is a light between thought and act"
+  // },
+  // {
+  //   id: 13,
+  //   text: "9 Bros of soldiers student line the beach is dry and Shallow at low tide the idea is to serve both edges straight the chase the dog down the street pages banned in class make a book try to trace the fine lines of the painting women for less than half of the group in the central part of town in the Rough needs work to polish used when secrets are set"
+  // },
+  // {
+  //   id: 14,
+  //   text: "who is the lord your left shoulder take the winding path with the like the size of the gas tank with degrees of his dirty face men the curve for you go out the risk of Delhi strained in Homeland history"
+  // },
+  // {
+  //   id: 15,
+  //   text: "the store was badly with coloured frocks the peace League met to discuss their plans the rise to Fame a person takes black paper scares so write with much care the quick fox jumped on the sleeping cat the novel The firehouse was bright cat on a status needed time brings as many changes the Purple time was 10 years old and plan and sometimes that"
+  // },
+  // {
+  //   id: 16,
+  //   text: "the smart hub not a hole in the sack the fish twisted in turn on the bed hug past tense and sorry but on the best this one time was first shot of perfect the beauty of the views on the young boy to blue fish swim in the think through the tower writer the same morning breakfast for pleasure"
+  // },
+  // {
+  //   id: 17,
+  //   text: "most of the news is easy for us to hear he used the late to make brass objects the pain on top of the pole revolved in the wind means Pie is a dish served to children the client gathered on each other night let it burn it gives us want and comfort a castle built from San fails to enter a child with save the day for us tax the strip of carpet to the moon floor next Tuesday we must put"
+  // },
+  // {
+  //   id: 18,
+  //   text: "every word and phrase he speaks is true he put his last cartridge into the gun inspired they took her kids from the public school derive the school straight into the word keep the hatch tight and the watch constant of the night pack and open the desk help the week to preserve their strength smile gets to friends"
+  // },
+  // {
+  //   id: 19,
+  //   text: "the state in early days the ship is tomorrow apart on the sharp relief Cygnus captain home the third week the white rose shimmer and the hot sun the lazy cow lay in the choreograph left the square stone over the fence the robot boy in the seven books at once up over the fence in the family King left the drug store inside"
+  // },
+  // {
+  //   id: 20,
+  //   text: "the person who played on the smooth Plains glory to the dark blue background is easy to tell the depth of a well these days the chicken leg is a rare dish rice is often served in round balls the juice of lemons makes fine punch the password from beside the part tracking and garbage 4 hours of study work faces a large size is starting"
+  // }
+];
